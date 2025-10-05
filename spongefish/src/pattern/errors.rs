5b26077ad5b543daac83f138273572dc30bcfcf9
yash25198 <ydumpala@gmail.com>
2025-10-05 23:31:23 +0530

    add errors

---
+++ spongefish/src/pattern/errors.rs
@@ -0,0 +1,25 @@
+use thiserror::Error;
+
+use super::Interaction;
+
+#[derive(Clone, Debug, Error, PartialEq, Eq)]
+pub enum PatternError {
+    #[error("Transcript is already finalized.")]
+    AlreadyFinalized,
+    #[error("No more expected interactions: {got}")]
+    NoMoreExpected { got: Interaction },
+    #[error("Unexpected interaction {got}, expected {expected}")]
+    UnexpectedInteraction { expected: Interaction, got: Interaction },
+    #[error("Missing Begin for {end}")]
+    MissingBegin { end: Interaction },
+    #[error("Mismatched begin and end: {begin}, {end}")]
+    MismatchedBeginEnd { begin: Interaction, end: Interaction },
+    #[error("Invalid kind {interaction} inside {begin}")]
+    InvalidKind { begin: Interaction, interaction: Interaction },
+    #[error("Maximum nesting depth exceeded: {limit}")]
+    DepthExceeded { limit: usize },
+    #[error("Transcript not finished, expecting {expected}")]
+    TranscriptNotFinished { expected: Interaction },
+}
+
+
