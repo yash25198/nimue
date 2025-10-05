0e97bd9054100fe050bd1a5593781b16c831943e
yash25198 <ydumpala@gmail.com>
2025-10-05 23:35:16 +0530

    fix bytes codec

--- spongefish/src/codecs/bytes.rs
+++ spongefish/src/codecs/bytes.rs
@@ -1,32 +1,35 @@
 use crate::{
     codecs::unit::Pattern as _,
-    pattern::{Label, Length, Pattern as _, PatternState},
+    pattern::{Label, Length, Pattern as _, PatternError, PatternState},
 };
 
 /// Traits for patterns that handle byte arrays in a transcript.
 pub trait Pattern {
-    fn public_bytes(&mut self, label: Label, size: usize);
-    fn message_bytes(&mut self, label: Label, size: usize);
-    fn challenge_bytes(&mut self, label: Label, size: usize);
+    fn public_bytes(&mut self, label: Label, size: usize) -> Result<(), PatternError>;
+    fn message_bytes(&mut self, label: Label, size: usize) -> Result<(), PatternError>;
+    fn challenge_bytes(&mut self, label: Label, size: usize) -> Result<(), PatternError>;
 }
 
 /// Implementation where `Unit = u8`
 impl Pattern for PatternState<u8> {
-    fn public_bytes(&mut self, label: Label, size: usize) {
-        self.begin_public::<u8>(label, Length::Fixed(size));
+    fn public_bytes(&mut self, label: Label, size: usize) -> Result<(), PatternError> {
+        self.begin_public::<u8>(label, Length::Fixed(size))?;
         self.public_units("units", size);
-        self.end_public::<u8>(label, Length::Fixed(size))
+        self.end_public::<u8>(label, Length::Fixed(size))?;
+        Ok(())
     }
 
-    fn message_bytes(&mut self, label: Label, size: usize) {
-        self.begin_message::<u8>(label, Length::Fixed(size));
+    fn message_bytes(&mut self, label: Label, size: usize) -> Result<(), PatternError> {
+        self.begin_message::<u8>(label, Length::Fixed(size))?;
         self.message_units("units", size);
-        self.end_message::<u8>(label, Length::Fixed(size))
+        self.end_message::<u8>(label, Length::Fixed(size))?;
+        Ok(())
     }
 
-    fn challenge_bytes(&mut self, label: Label, size: usize) {
-        self.begin_challenge::<u8>(label, Length::Fixed(size));
+    fn challenge_bytes(&mut self, label: Label, size: usize) -> Result<(), PatternError> {
+        self.begin_challenge::<u8>(label, Length::Fixed(size))?;
         self.challenge_units("units", size);
-        self.end_challenge::<u8>(label, Length::Fixed(size))
+        self.end_challenge::<u8>(label, Length::Fixed(size))?;
+        Ok(())
     }
 }
